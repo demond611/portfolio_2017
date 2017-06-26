003de2e9533308c3ca83bd32746f61d4979a5c90
Robert <demond611@gmail.com>
2017-06-25 23:08:11 -0500

    finished updating header javascript

--- app/assets/scripts/modules/RevealScroll.js
+++ app/assets/scripts/modules/RevealScroll.js
@@ -1,15 +1,38 @@
 import $ from 'jquery';
+import smoothScroll from 'jquery-smooth-scroll';
 
 class RevealScroll{
 
 	constructor(){
 		this.scroll = $('.site-header__scroll');
-		this.body = $('html, body');
+		this.events();
+	}
 
-		console.log("in the event");
-		this.body.animate({
-			scrollTop: this.scroll.offset().top
-		}, 1000);
+	events(){
+		this.scroll.click(this.addSmoothScrolling);
+		this.scroll.click(this.hideScroll);
+		this.scroll.click(this.showScroll);
+	}
+
+	addSmoothScrolling(){
+		this.scroll = $('.site-header__scroll');
+		$.smoothScroll({
+			direction: 'top'
+		});
+	}
+
+	hideScroll(){
+		$(document).scroll(function() {
+			var scroll = $('.site-header__scroll');
+			if ( $(window).scrollTop() === 0 ) scroll.fadeOut();
+		});
+	}
+
+	showScroll(){
+		$(document).scroll(function() {
+			var scroll = $('.site-header__scroll');
+			if ( $(window).scrollTop() !== 0 ) scroll.fadeIn();
+		});
 	}
 
 }
